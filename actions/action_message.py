f50b1e10b4ae2be3a49eaa9261f0fd1919a59800
ahmad.bappi <ahmad.bappi@expertflow.com>
2022-09-19 06:00:32 +0500

    ActionMessage event handling added

--- actions/action_message.py
+++ actions/action_message.py
@@ -1,10 +1,26 @@
 import logging
+from .utils.utility import Utility
+from .utils.events import slot
 
 
 class ActionMessage:
     def run(self, conversation, slots, dispatcher, metadata):
         self.log_info("intent received", conversation['id'])
-        return []
+        action_message = Utility.get_key(slots, 'actionMessage')
+
+        name = str(action_message['name'])
+        data = action_message['data']
+
+        events = []
+
+        agent_state = Utility.get_key(slots, 'agent_state', "not_requested")
+        if name == 'FIND_AGENT':
+            if agent_state == 'requested':
+                return []
+            events.append(slot.set('agent_state', 'requested'))
+
+        dispatcher.action(name, data)
+        return events
 
     @staticmethod
     def log_info(msg, conversation_id):
