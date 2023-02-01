02384988d3f521e899e7795a34998795586270a5
ahmad.bappi <ahmad.bappi@expertflow.com>
2023-02-01 11:14:30 +0500

    assign_resource_requested event added, agent_subscribed updated

--- actions/agent_subscribed.py
+++ actions/agent_subscribed.py
@@ -10,11 +10,12 @@ class AgentSubscribed:
         self.log_info("AGENT_SUBSCRIBED intent called", conversation_id)
 
         channel_sessions = Utility.get_channel_sessions(conversation)
+        agent_id = str(Utility.get_key(slots, 'agentParticipant')['participant']['id'])
 
         if not channel_sessions:
             self.log_info("Dispatching REVOKE_REQUEST bot-action, Customer has left", conversation_id)
             
-            dispatcher.action('REVOKE_REQUEST', {"reasonCode": "CANCELLED"})
+            dispatcher.action('REVOKE_REQUEST', self.get_revoke_resource_data(agent_id))
             return [slot.set('agent_state', Utility.create_agent_state('not_requested', None))]
 
         bot_participant = Utility.get_bot_participant(conversation)
@@ -24,6 +25,10 @@ class AgentSubscribed:
             dispatcher.action('CHANGE_PARTICIPANT_ROLE', {"participantId": bot_id, "role": role})
 
         return [slot.set('agent_state', Utility.create_agent_state('subscribed', None))]
+
+    @staticmethod
+    def get_revoke_resource_data(agent_id):
+        return {"reasonCode": "CANCELLED", "agent_id": agent_id}
 
     @staticmethod
     def log_info(msg, conversation_id):
