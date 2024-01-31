1ef0bc4f97b2fb908404fc9ea61e083deca5cbe3
Muhammad Zubair <muhammad.zubair@expertflow.com>
2024-01-31 10:59:15 +0300

    Changes for conversation Hold/Resume
    - Added conversation_paused.py
    - Added conversation_resumed.py
    - Added conversation_message.py

---
+++ actions/conversation_paused.py
@@ -0,0 +1,20 @@
+import logging
+
+from .utils.utility import Utility
+
+class ConversationPaused:
+    def run(self, conversation, slots, dispatcher, metadata):
+        room_info = (Utility.get_key(slots, 'cimEvent'))['roomInfo']
+        self.log_info("intent received", str(room_info['id']), conversation)
+
+        self.log_info("Dispatching the CONVERSATION_PAUSED message", str(room_info['id']), conversation)
+        dispatcher.text('The conversation is currently paused by the agent. We appreciate your patience,
+                        'the agent will get back to you as soon as possible.')
+
+        return []
+
+    @staticmethod
+    def log_info(msg, room_id, conversation):
+        conversation_id = str(None if not conversation else conversation['id'])
+        logging.info(
+            '[CONVERSATION_PAUSED] | room = [' + room_id + '] | conversation = [' + conversation_id + '] - ' + msg)
---
+++ actions/customer_message.py
@@ -0,0 +1,23 @@
+import logging
+
+from .utils.utility import Utility
+
+class CustomerMessage:
+    def run(self, conversation, slots, dispatcher, metadata):
+        room_info = (Utility.get_key(slots, 'cimEvent'))['roomInfo']
+        self.log_info("intent received", str(room_info['id']), conversation)
+
+        conversation_state = str(conversation['state'])
+
+        if conversation_state == "ON_HOLD":
+            self.log_info("Dispatching the CONVERSATION_HOLD message", str(room_info['id']), conversation)
+            dispatcher.text('The conversation is currently paused by the agent. We appreciate your patience,
+                            'the agent will get back to you as soon as possible.')
+
+        return []
+
+    @staticmethod
+    def log_info(msg, room_id, conversation):
+        conversation_id = str(None if not conversation else conversation['id'])
+        logging.info(
+            '[CUSTOMER_MESSAGE] | room = [' + room_id + '] | conversation = [' + conversation_id + '] - ' + msg)
