1f300f056b8b61c5e2eafda757171e5852b6adff
Rabi haider <rabi.haider@expertflow.com>
2024-08-23 07:37:11 +0000

    CIM-140085: CONSULT message fixed

--- actions/task_enqueued.py
+++ actions/task_enqueued.py
@@ -3,7 +3,17 @@ import logging
 
 class TaskEnqueued:
   def run(self, conversation, slots, dispatcher, metadata):
-      
-    dispatcher.text("You're routed to an agent, he/she will join in a moment.")
+    media = {}
+    active_media = slots.get('cimEvent',{}).get('data',{}).get('task',{}).get('activeMedia',[])
+    
+    if len(active_media) > 1:
+      for _media in active_media:
+        if _media.get('requestSession',{}).get('id','') ==  slots.get('cimEvent',{}).get('channelSession',{}).get('id',''):
+          media = _media
+    else:
+      media = active_media[0]
+    
+    if media.get('type',{}).get('direction','') != 'CONSULT':
+      dispatcher.text("You're routed to an agent, he/she will join in a moment.")
     
     return []
