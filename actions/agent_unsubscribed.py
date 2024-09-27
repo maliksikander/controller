6c04389f121a1b10d7e803e68a1917dbd503d62e
Muhammad Zubair <muhammad.zubair@expertflow.com>
2024-09-27 12:39:45 +0300

    Changes to FIND_AGENT on MRD_INTERRUPT

--- actions/agent_unsubscribed.py
+++ actions/agent_unsubscribed.py
@@ -21,7 +21,7 @@ class AgentUnSubscribed:
             reason_code = str((Utility.get_key(slots, 'cimEvent'))['data']['reason'])
 
             # If agent was unsubscribed by system, find another agent on this conversation
-            if routing_mode == 'PUSH' and (reason_code == 'FORCED_LOGOUT' or reason_code == 'SLA_EXPIRED'):
+            if routing_mode == 'PUSH' and (reason_code == 'FORCED_LOGOUT' or reason_code == 'SLA_EXPIRED' or reason_code == 'MRD_INTERRUPTED'):
                 self.log_info('Dispatching FIND_AGENT', str(room_info['id']), conversation)
                 dispatcher.action('FIND_AGENT', {'lastAgentId': API.get_last_agent(conversation['customer']['_id'])})
 
